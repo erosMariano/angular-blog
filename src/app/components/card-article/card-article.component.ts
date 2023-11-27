233c9cd884583f149a6db3b3957c60da51880e15
erosMariano <erosmariano1@gmail.com>
2023-11-26 21:51:32 -0300

    feat: section blog card

---
+++ src/app/components/card-article/card-article.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-card-article',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './card-article.component.html',
+  styleUrl: './card-article.component.scss'
+})
+export class CardArticleComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CardArticleComponent } from '../../components/card-article/card-article.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CardArticleComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
