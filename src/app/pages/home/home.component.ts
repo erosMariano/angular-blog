b5031fca5077ffb4f8b47e1c575e776f0fdb7505
erosMariano <erosmariano1@gmail.com>
2023-11-26 22:08:00 -0300

    feat: footer created

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardArticleComponent } from '../../components/card-article/card-article.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, CardArticleComponent],
+  imports: [CommonModule, CardArticleComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
